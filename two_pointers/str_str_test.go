c0d5186b7bc464f7bd46dbb31ab3973336527eaf
jhlord163 <jhlord@163.com>
2019-10-29 22:10:20 +0800

    TwoPointers

---
+++ two_pointers/str_str_test.go
@@ -0,0 +1,15 @@
+package two_pointers
+
+import (
+	"testing"
+)
+
+func TestStrStr(t *testing.T) {
+	var haystack, needle string
+	var h, r int
+
+	haystack = "a"
+	needle = "a"
+	r = strStr(haystack, needle)
+	t.Logf("%t haystack=%s needle=%s h=%d r=%d", r==h, haystack, needle, h, r)
+}
