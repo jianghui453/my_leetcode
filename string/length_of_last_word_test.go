3fa5e6ef759790911e6c528c3aa1f39b0a053ab4
jhlord <jhlord@163.com>
2019-11-07 19:59:30 +0800

    Length of last word

---
+++ string/length_of_last_word_test.go
@@ -0,0 +1,15 @@
+package string
+
+import (
+	"testing"
+)
+
+func TestLengthOfLastWord(t *testing.T) {
+	var s string
+	var h, r int
+
+	s = "hello world"
+	h = 5
+	r = lengthOfLastWord(s)
+	t.Logf("%t s=%s h=%d r=%d", r==h, s, h, r)
+}
