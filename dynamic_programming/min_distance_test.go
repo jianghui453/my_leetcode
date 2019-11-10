af8773908d81b640b5e8e26ee7d67df0796fd1b3
jhlord <jhlord@163.com>
2019-11-10 16:20:50 +0800

    Edit distance

--- dynamic_programming/min_distance_test.go
+++ dynamic_programming/min_distance_test.go
@@ -1,4 +1,4 @@
-package min_distance
+package dynamic_programming
 
 import "testing"
 
@@ -10,17 +10,17 @@ func TestMinDistance(t *testing.T) {
 	word2 = "ros"
 	hope = 3
 	ret = minDistance(word1, word2)
-	t.Logf("word1=%s word2=%s hope=%d ret=%d", word1, word2, hope, ret)
+	t.Logf("%t word1=%s word2=%s hope=%d ret=%d", ret==hope, word1, word2, hope, ret)
 
 	word1 = "intention"
 	word2 = "execution"
 	hope = 5
 	ret = minDistance(word1, word2)
-	t.Logf("word1=%s word2=%s hope=%d ret=%d", word1, word2, hope, ret)
+	t.Logf("%t word1=%s word2=%s hope=%d ret=%d", ret==hope, word1, word2, hope, ret)
 
 	word1 = "sea"
 	word2 = "eat"
 	hope = 2
 	ret = minDistance(word1, word2)
-	t.Logf("word1=%s word2=%s hope=%d ret=%d", word1, word2, hope, ret)
+	t.Logf("%t word1=%s word2=%s hope=%d ret=%d", ret==hope, word1, word2, hope, ret)
 }
