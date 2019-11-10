37a22f7b1aa8f0c899120df97ec2870199212479
jhlord <jhlord@163.com>
2019-11-10 14:44:09 +0800

    Unique path II

--- dynamic_programming/unique_path_with_obstacle.go
+++ dynamic_programming/unique_path_with_obstacle.go
@@ -36,15 +36,15 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	if n == 0 {
 		return 0
 	}
+	if obstacleGrid[0][0] == 1 {
+		return 0
+	}
 	
 	dp := make([][]int, m)
-	for i := 0; i < m; i ++ {
+	for i := 0; i < m; i++ {
 		dp[i] = make([]int, n)
-	}
-	dp[0][0] = 1
-
-	for i := 0; i < m; i ++ {
-		for j := 0; j < n; j ++ {
+		dp[0][0] = 1
+		for j := 0; j < n; j++ {
 			if obstacleGrid[i][j] == 1 {
 				continue
 			} 
@@ -55,6 +55,7 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 			if j > 0 {
 				dp[i][j] += dp[i][j-1]
 			}
+
 		}
 	}
 	
--- dynamic_programming/unique_path_with_obstacle_test.go
+++ dynamic_programming/unique_path_with_obstacle_test.go
@@ -6,12 +6,13 @@ func TestUniquePathWithObstacle(t *testing.T) {
 	var obstacleGrid [][]int
 	var h, r int
 
-	obstacleGrid = [][]int{
-		{0,0,0},
-		{0,1,0},
-		{0,0,0},
-	}
+	obstacleGrid = [][]int{{0,0,0}, {0,1,0}, {0,0,0}}
 	h = 2
+	r = uniquePathsWithObstacles(obstacleGrid)
+	t.Logf("h=%d r=%d", h, r)
+
+	obstacleGrid = [][]int{{1}}
+	h = 0
 	r = uniquePathsWithObstacles(obstacleGrid)
 	t.Logf("h=%d r=%d", h, r)
 }
