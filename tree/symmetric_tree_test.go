750d141d8c77491a8505f214bbb059f586964810
jhlord <jhlord@163.com>
2019-11-25 19:27:22 +0800

    Symmetric Tree

---
+++ tree/symmetric_tree_test.go
@@ -0,0 +1,95 @@
+package tree
+
+import "testing"
+
+func TestIsSymmetric(t *testing.T) {
+	var root *TreeNode
+	var h, r bool
+
+	root = &TreeNode{
+	   1,
+	   &TreeNode{
+	       2,
+	       nil,
+	       &TreeNode{
+	           3,
+	           nil,
+	           nil,
+	       },
+	   },
+	   &TreeNode{
+	       2,
+	       nil,
+	       &TreeNode{
+	           3,
+	           nil,
+	           nil,
+	       },
+	   },
+	}
+	h = false
+	r = isSymmetric(root)
+	t.Logf("%t h=%t r=%t", h==r, h, r)
+	
+	root = &TreeNode{
+	  1,
+	  &TreeNode{
+	      2,
+	      &TreeNode{
+	          3,
+	          nil,
+	          nil,
+	      },
+	      &TreeNode{
+	          4,
+	          nil,
+	          nil,
+	      },
+	  },
+	  &TreeNode{
+	      2,
+	      &TreeNode{
+	          4,
+	          nil,
+	          nil,
+	      },
+	      &TreeNode{
+	          3,
+	          nil,
+	          nil,
+	      },
+	  },
+	}
+	h = true
+	r = isSymmetric(root)
+	t.Logf("%t h=%t r=%t", h==r, h, r)
+
+	root = &TreeNode{
+		2,
+		&TreeNode{
+			3,
+			&TreeNode{
+				4,
+				nil,
+				nil,
+			},
+			&TreeNode{
+				5,
+				nil,
+				nil,
+			},
+		},
+		&TreeNode{
+			3,
+			&TreeNode{
+				5,
+				nil,
+				nil,
+			},
+			nil,
+		},
+	}
+	h = false
+	r = isSymmetric(root)
+	t.Logf("%t h=%t r=%t", h == r, h, r)
+}
