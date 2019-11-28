0d127a6f7c89632bfd443c4511d38afd0527b9ce
jhlord <jhlord@163.com>
2019-11-28 17:20:08 +0800

    Pascal`s Triangle

---
+++ array/pascal_s_triangle_ii_test.go
@@ -0,0 +1,20 @@
+package array
+
+import (
+	"testing"
+)
+
+func TestGetRow(t *testing.T) {
+	var rowIndex int
+	var hope, ret []int
+
+	rowIndex = 4
+	ret = getRow(rowIndex)
+	hope = []int{1,4,6,4,1}
+	t.Logf("\nhope=%v \n ret=%v", hope, ret)
+
+	rowIndex = 0
+	ret = getRow(rowIndex)
+	hope = []int{1}
+	t.Logf("\nhope=%v \n ret=%v", hope, ret)
+}
