d2b345addf0ec4d32067748bcdc9a12128ed70a0
jhlord <jhlord@163.com>
2019-12-10 11:34:53 +0800

    Longest Increasing Subsequence

---
+++ dynamic_programming/longest_increasing_subsequence.go
@@ -0,0 +1,60 @@
+// 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+
+// 示例:
+
+// 输入: [10,9,2,5,3,7,101,18]
+// 输出: 4 
+// 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+// 说明:
+
+// 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+// 你算法的时间复杂度应该为 O(n2) 。
+// 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+
+package dynamic_programming
+
+import (
+	// "fmt"
+)
+
+func lengthOfLIS(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+
+	s := make([]int, l)
+	dp := make([]int, l)
+	dp[0] = 1
+	ret := 1
+	for i := 1; i < l; i++ {
+		dp[i] = 1
+
+		for j := i-1; j >= 0; j-- {
+			if nums[i] > nums[s[j]] {
+				dp[i] = max(dp[i], dp[s[j]]+1)
+				break
+			}
+		}
+
+		s[i] = i
+		for j := i-1; j >= 0; j-- {
+			if dp[s[j]] > dp[s[j+1]] {
+				s[j], s[j+1] = s[j+1], s[j]
+			} else {
+				break
+			}
+		}
+
+		ret = max(ret, dp[i])
+	}
+	
+	return ret
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
---
+++ dynamic_programming/longest_increasing_subsequence_test.go
@@ -0,0 +1,31 @@
+package dynamic_programming
+
+import (
+	"testing"
+)
+
+func Test_lengthOfLIS(t *testing.T) {
+	var nums []int
+	var hope, ret int
+
+	// nums = []int{10,9,2,5,3,7,101,18}
+	// hope = 4
+	// ret = lengthOfLIS(nums)
+	// t.Logf("%t nums=%v hope=%d ret=%d", ret == hope, nums, hope, ret)
+
+	nums = []int{10,9,2,4,5,3,7,101,18}
+	hope = 5
+	ret = lengthOfLIS(nums)
+	t.Logf("%t nums=%v hope=%d ret=%d", ret == hope, nums, hope, ret)
+
+	
+	// nums = []int{0}
+	// hope = 1
+	// ret = lengthOfLIS(nums)
+	// t.Logf("%t nums=%v hope=%d ret=%d", ret == hope, nums, hope, ret)
+
+	// nums = []int{2, 2}
+	// hope = 1
+	// ret = lengthOfLIS(nums)
+	// t.Logf("%t nums=%v hope=%d ret=%d", ret == hope, nums, hope, ret)
+}
