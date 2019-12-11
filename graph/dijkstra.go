41961e013f27de53f902bf88805b6d17c3b1fdfa
jhlord <jhlord@163.com>
2019-12-11 11:03:43 +0800

    Dijkstra Algotithm

---
+++ graph/dijkstra.go
@@ -0,0 +1,74 @@
+
+// 迪杰斯特拉算法
+// 获取大小为V的图的最小生成树
+// 把起始点的距离值设为0
+// （1）找到未被标记的顶点中离已被标记的顶点u最近的一个顶点v
+// （2）更新这个顶点的距离值（u的距离值加u到v的距离）
+// （3）如果还有未被标记的顶点回到（1）
+
+package dijkstra
+
+import (
+	"fmt"
+	// "math"
+)
+
+func dijkstra(V int, graph [][]int) []int {
+	spt := make([]int, V)
+
+	for flag := true; flag; {
+		var (
+			idx int
+			min int
+		)
+
+		flag = false
+		for i := 0; i < V; i++ {
+			if i != 0 && spt[i] == 0 {
+				flag = true
+				continue
+			}
+
+			for j := 1; j < V; j++ {
+				if spt[j] == 0 && graph[i][j] > 0 && (min == 0 || spt[i]+graph[i][j] < min) {
+					min = spt[i]+graph[i][j]
+					idx = j
+				}
+			}
+		}
+
+		spt[idx] = min
+	}
+
+	return spt
+}
+
+// func dijkstra(V int, graph [][]int) []int {
+// 	sptMark := make([]bool, V) // 记录点是否已经标记了
+// 	spt := make([]int, V)      // 记录点的最小距离值
+// 	for i := 0; i < V; i++ {
+// 		spt[i] = -1
+// 	}
+// 	// 原点的距离值标记为0
+// 	spt[0] = 0
+// 	for i := 1; i < V; i++ {
+// 		// 查找能达到的，距离值最小的点
+// 		minIdx := -1
+// 		minVal := math.MaxInt64
+// 		for i := 0; i < V; i++ {
+// 			if sptMark[i] == false && spt[i] != -1 && spt[i] < minVal {
+// 				minIdx = i
+// 				minVal = spt[i]
+// 			}
+// 		}
+// 		// 把这个点能达到的点标上距离值
+// 		sptMark[minIdx] = true
+// 		for i := 0; i < V; i++ {
+// 			if sptMark[i] == false && graph[minIdx][i] > 0 &&
+// 				(spt[i] == -1 || spt[minIdx]+graph[minIdx][i] < spt[i]) {
+// 				spt[i] = spt[minIdx] + graph[minIdx][i]
+// 			}
+// 		}
+// 	}
+// 	return spt
+// }
--- graph/dijkstra_test.go
+++ graph/dijkstra_test.go
@@ -3,7 +3,10 @@ package dijkstra
 import "testing"
 
 func TestDijkstra(t *testing.T) {
-	graph := [][]int{
+	var graph [][]int
+	var hope, ret []int
+
+	graph = [][]int{
 		{0, 4, 0, 0, 0, 0, 0, 8, 0},
 		{4, 0, 8, 0, 0, 0, 0, 11, 0},
 		{0, 8, 0, 7, 0, 4, 0, 0, 2},
@@ -14,6 +17,7 @@ func TestDijkstra(t *testing.T) {
 		{8, 11, 0, 0, 0, 0, 1, 0, 7},
 		{0, 0, 2, 0, 0, 0, 6, 7, 0},
 	}
-	spt := dijkstra(9, graph)
-	t.Logf("graph=%v \nspt=%v", graph, spt)
+	hope = []int{0, 4, 12, 19, 21, 11, 9, 8, 14}
+	ret = dijkstra(9, graph)
+	t.Logf("graph=%v \nhope=%v \n ret=%v", graph, hope, ret)
 }
