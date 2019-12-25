79b37b369716b1299f7c8c97ddb06821f4b6d424
jianghui453 <jianghui453@foxmail.com>
2019-12-25 19:44:58 +0800

    remove linked list elements

---
+++ linked_list/remove_linked_list_elements.go
@@ -0,0 +1,31 @@
+// 删除链表中等于给定值 val 的所有节点。
+
+// 示例:
+
+// 输入: 1->2->6->3->4->5->6, val = 6
+// 输出: 1->2->3->4->5
+
+package linked_list
+
+func removeElements(head *ListNode, val int) *ListNode {
+    var (
+		tail, h *ListNode = head, head
+	)
+
+	for h != nil {
+		if h.Val == val {
+			if head == h {
+				head, tail, h = head.Next, tail.Next, h.Next
+			} else {
+				h = h.Next
+			}
+		} else {
+			tail.Next, tail, h = h, h, h.Next
+		}
+	}
+
+	if tail != nil {
+		tail.Next = nil
+	}
+	return head
+}
---
+++ linked_list/remove_linked_list_elements_test.go
@@ -0,0 +1,42 @@
+package linked_list
+
+import (
+	"testing"
+)
+
+func Test_removeElements(t *testing.T) {
+	var (
+		head *ListNode
+		val int
+		hope, ret []int
+	)
+
+	head = &ListNode{1,&ListNode{2,&ListNode{6,&ListNode{3,&ListNode{4,&ListNode{5,&ListNode{6,nil}}}}}}}
+	val = 6
+	hope = []int{1, 2, 3, 4, 5}
+	ret = toArray(removeElements(head, val))
+	t.Log(ret)
+	t.Log(hope)
+
+	head = &ListNode{1,&ListNode{2,&ListNode{6,&ListNode{3,&ListNode{4,&ListNode{5,&ListNode{6,nil}}}}}}}
+	val = 1
+	hope = []int{2, 6, 3, 4, 5, 6}
+	ret = toArray(removeElements(head, val))
+	t.Log(ret)
+	t.Log(hope)
+
+	head = &ListNode{1,nil}
+	val = 1
+	hope = []int{}
+	ret = toArray(removeElements(head, val))
+	t.Log(ret)
+	t.Log(hope)
+}
+
+func toArray(head *ListNode) []int {
+	ret := make([]int, 0)
+	for head != nil {
+		ret, head = append(ret, head.Val), head.Next
+	}
+	return ret
+}
