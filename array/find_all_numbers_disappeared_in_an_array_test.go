ac4e7cca2c6fdfe9a426cb314135a43fd525d749
jianghui453 <jianghui453@foxmail.com>
2019-12-31 11:27:29 +0800

    find all numbers disappeared in an array

---
+++ array/find_all_numbers_disappeared_in_an_array_test.go
@@ -0,0 +1,32 @@
+package array
+
+import (
+	"testing"
+)
+
+func Test_findDisappearedNumbers(t *testing.T) {
+	var (
+		nums, hope, ret []int
+	)
+
+	nums = []int{4,3,2,7,8,2,3,1}
+	hope = []int{5,6}
+	ret = findDisappearedNumbers(nums)
+	t.Log(nums)
+	t.Log(hope)
+	t.Log(ret)
+
+	nums = []int{1,1,1,1,1,1,1}
+	hope = []int{2,3, 4, 5, 6, 7}
+	ret = findDisappearedNumbers(nums)
+	t.Log(nums)
+	t.Log(hope)
+	t.Log(ret)
+
+	nums = []int{1}
+	hope = []int{}
+	ret = findDisappearedNumbers(nums)
+	t.Log(nums)
+	t.Log(hope)
+	t.Log(ret)
+}
