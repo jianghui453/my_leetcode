5f9fae3f458a1060ef6f1ff05da1ef4f6d13e270
jianghui453 <jianghui453@foxmail.com>
2020-01-02 10:11:42 +0800

    first missing positive

--- array/first_missing_positive.go
+++ array/first_missing_positive.go
@@ -18,29 +18,121 @@
 
 package array
 
+import (
+	
+)
+
+// class Solution {
+// 	public int firstMissingPositive(int[] nums) {
+// 		int n = nums.length;
+
+// 		// 基本情况
+// 		int contains = 0;
+// 		for (int i = 0; i < n; i++)
+// 		if (nums[i] == 1) {
+// 			contains++;
+// 			break;
+// 		}
+
+// 		if (contains == 0)
+// 		return 1;
+
+// 		// nums = [1]
+// 		if (n == 1)
+// 		return 2;
+
+// 		// 用 1 替换负数，0，
+// 		// 和大于 n 的数
+// 		// 在转换以后，nums 只会包含
+// 		// 正数
+// 		for (int i = 0; i < n; i++)
+// 		if ((nums[i] <= 0) || (nums[i] > n))
+// 			nums[i] = 1;
+
+// 		// 使用索引和数字符号作为检查器
+// 		// 例如，如果 nums[1] 是负数表示在数组中出现了数字 `1`
+// 		// 如果 nums[2] 是正数 表示数字 2 没有出现
+// 		for (int i = 0; i < n; i++) {
+// 		int a = Math.abs(nums[i]);
+// 		// 如果发现了一个数字 a - 改变第 a 个元素的符号
+// 		// 注意重复元素只需操作一次
+// 		if (a == n)
+// 			nums[0] = - Math.abs(nums[0]);
+// 		else
+// 			nums[a] = - Math.abs(nums[a]);
+// 		}
+
+// 		// 现在第一个正数的下标
+// 		// 就是第一个缺失的数
+// 		for (int i = 1; i < n; i++) {
+// 		if (nums[i] > 0)
+// 			return i;
+// 		}
+
+// 		if (nums[0] > 0)
+// 		return n;
+
+// 		return n + 1;
+// 	}
+// }
+// func firstMissingPositive(nums []int) int {
+// 	hash := make(map[int]bool)
+// 	max := 0
+// 	for i := 0; i < len(nums); i++ {
+// 		if nums[i] <= 0 {
+// 			continue
+// 		}
+// 		if nums[i] > max {
+// 			max = nums[i]
+// 		}
+// 		if _, ok := hash[nums[i]]; !ok {
+// 			hash[nums[i]] = true
+// 		}
+// 	}
+// 	if max == 0 {
+// 		return 1
+// 	}
+
+// 	var i int
+// 	for i = 1; i < max; i++ {
+// 		if _, ok := hash[i]; !ok {
+// 			return i
+// 		}
+// 	}
+// 	return i + 1
+// }
+
 func firstMissingPositive(nums []int) int {
-	hash := make(map[int]bool)
-	max := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] <= 0 {
-			continue
+	var (
+		l int = len(nums)
+	)
+
+	for i := 0; i < l; i++ {
+		if nums[i] == 0 {
+			nums[i]--
 		}
-		if nums[i] > max {
-			max = nums[i]
-		}
-		if _, ok := hash[nums[i]]; !ok {
-			hash[nums[i]] = true
-		}
-	}
-	if max == 0 {
-		return 1
 	}
 
-	var i int
-	for i = 1; i < max; i++ {
-		if _, ok := hash[i]; !ok {
-			return i
+	for i := 0; i < l; i++ {
+		if nums[i] > 0 {
+			mark(nums, nums[i]-1, l)
 		}
 	}
-	return i + 1
+
+	for i := 0; i < l; i++ {
+		if nums[i] != 0 {
+			return i+1
+		}
+	}
+
+	return l+1
+}
+
+func mark(nums []int, idx, l int) {
+	if idx < 0 || idx >= l {
+		return
+	}
+
+	nums[idx], idx = 0, nums[idx]-1
+	mark(nums, idx, l)
 }
--- array/first_missing_positive_test.go
+++ array/first_missing_positive_test.go
@@ -10,21 +10,43 @@ func TestFirstMissingPositive(t *testing.T) {
 
 	nums = []int{1, 2, 0}
 	h = 3
+	t.Log(nums)
 	r = firstMissingPositive(nums)
-	t.Logf("nums=%v h=%d r=%d\n", nums, h, r)
+	t.Logf("%t nums=%v h=%d r=%d\n", r == h, nums, h, r)
 
 	nums = []int{3, 4, -1, 1}
 	h = 2
+	t.Log(nums)
 	r = firstMissingPositive(nums)
-	t.Logf("nums=%v h=%d r=%d\n", nums, h, r)
+	t.Logf("%t nums=%v h=%d r=%d\n", r == h, nums, h, r)
 
 	nums = []int{7, 8, 9, 11, 12}
 	h = 1
+	t.Log(nums)
 	r = firstMissingPositive(nums)
-	t.Logf("nums=%v h=%d r=%d\n", nums, h, r)
+	t.Logf("%t nums=%v h=%d r=%d\n", r == h, nums, h, r)
+
+	nums = []int{2}
+	h = 1
+	t.Log(nums)
+	r = firstMissingPositive(nums)
+	t.Logf("%t nums=%v h=%d r=%d\n", r == h, nums, h, r)
+
+	nums = []int{1}
+	h = 2
+	t.Log(nums)
+	r = firstMissingPositive(nums)
+	t.Logf("%t nums=%v h=%d r=%d\n", r == h, nums, h, r)
+
+	nums = []int{1, 2}
+	h = 3
+	t.Log(nums)
+	r = firstMissingPositive(nums)
+	t.Logf("%t nums=%v h=%d r=%d\n", r == h, nums, h, r)
 
 	nums = []int{}
 	h = 1
+	t.Log(nums)
 	r = firstMissingPositive(nums)
-	t.Logf("nums=%v h=%d r=%d\n", nums, h, r)
+	t.Logf("%t nums=%v h=%d r=%d\n", r == h, nums, h, r)
 }
